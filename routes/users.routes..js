7506d4f7a4c249e207843fbfd8374b93749f0e99
Scutun <evplotnikov04@ya.ru>
2024-12-07 19:47:14 +0300

    feat: added routes for registration and login

--- routes/users.routes..js
+++ routes/users.routes..js
@@ -3,4 +3,8 @@ const router = new Router()
 const userController = require("../controllers/user.controller")
 const checkToken = require("../middleware/checkToken")
 
+router.post("/logIn", userController.logUser)
+
+router.post("/create", userController.createUser)
+
 module.exports = router
