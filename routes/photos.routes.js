7553e000228249e83b349bedd753e5ed40fbaa66
Scutun <evplotnikov04@ya.ru>
2024-12-13 18:49:46 +0300

    fix: changed sending of url for photos

--- routes/photos.routes.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express")
-const router = new Router()
-const photoController = require("../controllers/photos.contoller")
-
-router.get("/projects/photos/url/:photo", photoController.getProjectPhotoUrl)
-
-router.get("/architects/photos/url/:photo", photoController.getArchitectPhotoUrl)
-
-module.exports = router
